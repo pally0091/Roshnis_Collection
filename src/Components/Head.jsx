6498989b8fe84f6c4bda850f097d514f0537db5a
Debashis Roy <goodquote39@hotmail.com>
2023-10-10 09:32:56 +0600

    nav responsive issue fix

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import logo from "../assets/Logo.png";
@@ -6,7 +7,7 @@ import { BsFacebook, BsWhatsapp } from "react-icons/bs";
 const Head = () => {
   return (
     <div className="flex flex-row justify-evenly items-center">
-      <div className="w-1/3">
+      <div className="w-[70%] md:w-1/3 lg:w-1/3">
         <img
           src={logo}
           alt=""
@@ -15,7 +16,8 @@ const Head = () => {
       <div className="w-1/3 md:flex lg:flex justify-end hidden ">
         <a
           className="mx-2 text-2xl shadow-lg shadow-stone-700 rounded-full hover:shadow-none transition-all duration-500"
-          href=""
+          href="https://www.facebook.com/roshnitc2021"
+          target="_blank"
         >
           <BsFacebook></BsFacebook>
         </a>
